355392a414f4521fdec7e888dd67b100d4d3ebdf
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-05-12 09:09:34 +0200

    add email.php

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php
+$email = "sebastian.scholl@gmx.at";
+$textNr = 4;
+$textWidth = imagefontwidth($textNr) * strlen($email);
+$textHeight = imagefontheight($textNr);
+
+header("Content-type: image/png");
+
+$image = imagecreate($textWidth, $textHeight);
+
+$backgroundColor = imagecolorallocate($image, 222, 222, 222);
+$textColor = imagecolorallocate($image, 0, 0, 0);
+imagestring($image, $textNr, 0, 0, $email, $textColor);
+
+imagepng($image);
+
+imagedestroy($image);
